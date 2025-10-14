f0817042dbb278b7e9ce7df3d8cfc8dcc9db2c57
SH_note <sanmhyo@gmail.com>
2025-10-14 13:43:02 +0900

    run 복사하여 AutoRun class 생성

--- boy.py
+++ boy.py
@@ -24,6 +24,30 @@ def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 
+class AutoRun:
+
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self, e):
+        if right_down(e) or left_up(e):
+            self.boy.dir = self.boy.face_dir = 1
+        elif left_down(e) or right_up(e):
+            self.boy.dir = self.boy.face_dir = -1
+
+    def exit(self, e):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+        self.boy.x += self.boy.dir * 5
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x, self.boy.y, 100, 100)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y, 100, 100)
+
 
 class Run:
 
